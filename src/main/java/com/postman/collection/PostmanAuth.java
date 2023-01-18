c2decb4b9efb67d1c9cd92b729e7f0515d11fde4
Bryan Cross <bryanrkcross@gmail.com>
2023-01-18 09:11:11 -0600

    Removed PostmanAPIKey

--- src/main/java/com/postman/collection/PostmanAuth.java
+++ src/main/java/com/postman/collection/PostmanAuth.java
@@ -2,11 +2,15 @@ package com.postman.collection;
 
 import com.google.gson.*;
 import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class PostmanAuth implements IPostmanCollectionElement {
     private transient String key = UUID.randomUUID().toString();    
     private String type = "";
-    private PostmanAPIKey[] apikey = null;
+    private PostmanVariable[] apikey = null;
+
     
     
     public String getType() {
@@ -39,15 +43,89 @@ public class PostmanAuth implements IPostmanCollectionElement {
         this.type = type;
     }
 
-    public PostmanAPIKey[] getApikey() {
+    public PostmanVariable[] getApikey() {
         return apikey;
     }
 
-    public void setApikey(PostmanAPIKey[] apikey) {
+    public void setApikey(PostmanVariable[] apikey) {
         this.apikey = apikey;
     }
 
-    public PostmanAuth(String type, PostmanAPIKey[] apikey) {
+    public void setAPIKey(String value, String type) {
+
+    }
+
+    public PostmanVariable getAPIKey() {
+        return null;
+    }
+
+    public void setAPIValue(String value, String type) {
+        
+    }
+
+    public PostmanVariable getAPIValue() {
+        return this.getAPIElement("value");
+    }
+
+    public PostmanVariable getAPIElement(String key) {
+        List<PostmanVariable> liElems;
+        if(this.apikey == null)
+        {
+            liElems = new ArrayList<PostmanVariable>(Arrays.asList(new PostmanVariable[0]));
+        }
+        else
+          {
+            liElems = new ArrayList<PostmanVariable>(Arrays.asList(this.apikey));
+          }
+
+          for(int i = 0; i < liElems.size(); i++)
+          {
+            if(liElems.get(i).getKey().equals(key)) {
+                return liElems.get(i);
+            }
+          }
+          return null;
+    }
+
+    private void setAPIElement(String key, PostmanVariable newElement) {
+        PostmanVariable apiElem = this.getAPIElement(key);
+        List<PostmanVariable> liElems;
+        if(apiElem != null)
+        {
+            apiElem = newElement;
+            return;
+        }
+        
+        if(this.apikey == null)
+        {
+            liElems = new ArrayList<PostmanVariable>(Arrays.asList(new PostmanVariable[0]));
+        }
+        else
+          {
+            liElems = new ArrayList<PostmanVariable>(Arrays.asList(this.apikey));
+          }
+        liElems.add(newElement);
+        this.apikey = liElems.toArray(new PostmanVariable[0]);
+    }
+
+    
+
+    public void addApiKey(String value, String type) {
+        PostmanVariable newKey = new PostmanVariable("key",value);
+        newKey.setType(type);
+        this.setAPIElement("key", newKey);
+        
+    }
+
+    
+
+    public void addApiValue(String value, String type ) {
+        PostmanVariable newValue = new PostmanVariable(value, type);
+        newValue.setKey("value");
+        this.setAPIElement("value",newValue);
+    }
+
+    public PostmanAuth(String type, PostmanVariable[] apikey) {
         this.type = type;
         this.apikey = apikey;
     }
