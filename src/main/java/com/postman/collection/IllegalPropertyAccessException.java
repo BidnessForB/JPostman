0041cd3cbfda2ae52f7860f23d3164d19485e3f0
Bryan Cross <bryanrkcross@gmail.com>
2023-01-21 18:38:16 -0600

    Adding exception

---
+++ src/main/java/com/postman/collection/IllegalPropertyAccessException.java
@@ -0,0 +1,7 @@
+package com.postman.collection;
+
+public class IllegalPropertyAccessException extends Exception  {
+    public IllegalPropertyAccessException(String message) {
+        super(message);
+    }
+}
