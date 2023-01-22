0d5dcf96730a81b5e4198d0ed23cb1aaa1a56f9e
Bryan Cross <bryanrkcross@gmail.com>
2023-01-21 18:50:02 -0600

    Adding exception

--- src/main/java/com/postman/collection/PostmanVariable.java
+++ src/main/java/com/postman/collection/PostmanVariable.java
@@ -1,5 +1,10 @@
 package com.postman.collection;
-
+/**
+ * 
+ * Utility class to store key-value pairs with additional, optional <code>description</code> and <code>type</code> properties
+ * 
+ * 
+ */
 public class PostmanVariable extends PostmanCollectionElement {
     private String key = "";
     private String value = "";
@@ -8,17 +13,39 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
-     * @return String
+     * 
+     * Returns the Postman token for a variable, eg. the <code>key</code> surrounded by double curly braces <code>{{}}</code>
+     * 
+     * 
+     * @return String  The postman token
      */
     public String getToken() {
         return "{{" + key + "}}";
     }
 
+
+/**
+ * 
+ * Constructs a PostmanVariable with the specified properties.  The <code>type</code> property is set to <code>null</code>
+ * 
+ * @param key Value for the <code>key</code> property
+ * @param value Value for the <code>value</code> property
+ * @param description Value for the <code>description</code> proptery
+ */
+
     public PostmanVariable(String key, String value, String description) {
         this(key, value, description, null);
 
     }
-
+/**
+ * 
+ * Constructs a PostmanVariable with all specified properties.
+ * 
+ * @param key Value for the <code>key</code> property
+ * @param value Value for the <code>value</code> property
+ * @param description Value for the <code>description</code> property
+ * @param type Value for the <code>type</code> property
+ */
     public PostmanVariable(String key, String value, String description, String type) {
         this.key = key;
         this.value = value;
@@ -26,20 +53,36 @@ public class PostmanVariable extends PostmanCollectionElement {
         this.type = type;
     }
 
+    /**
+ * 
+ * Constructs a PostmanVariable with with just <code>key</code> and <code>value</code> properties
+ * 
+ * @param key Value for the <code>key</code> property
+ * @param value Value for the <code>value</code> property
+ */
+    
     public PostmanVariable(String key, String value) {
         this(key, value, null, null);
     }
 
     
     /** 
+     * 
+     * returns the value of the <code>key</code> property
+     * 
      * @return String
      */
+    @Override
     public String getKey() {
         return key;
     }
 
     
     /** 
+     * 
+     * Sets the <code>description</code> property
+     * 
+     * 
      * @param desc
      */
     public void setDescription(String desc) {
@@ -48,6 +91,9 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
+     * 
+     * Returns the <code>description</code> property, or null if it is not set
+     * 
      * @return String
      */
     public String getDescription() {
@@ -56,6 +102,9 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
+     * 
+     * Sets the <code>key</code> value for this property
+     * 
      * @param key
      */
     public void setKey(String key) {
@@ -64,6 +113,8 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
+     * 
+     * Sets the <code>value</code> property for this property
      * @return String
      */
     public String getValue() {
@@ -72,7 +123,10 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
-     * @param value
+     * 
+     * Sets the <code>value</code> property for this variable
+     * 
+     * @param value  The value
      */
     public void setValue(String value) {
         this.value = value;
@@ -80,7 +134,10 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
-     * @return String
+     * 
+     * Returns the <code>type</code> property for this variable
+     * 
+     * @return String the type
      */
     public String getType() {
         return this.type;
@@ -88,7 +145,10 @@ public class PostmanVariable extends PostmanCollectionElement {
 
     
     /** 
-     * @param type
+     * 
+     * Sets the <code>type</code> property for this variable
+     * 
+     * @param type  The type, e.g., "string" or "boolean"
      */
     public void setType(String type) {
         this.type = type;
